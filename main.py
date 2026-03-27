23dca4204cf4cf227b62617eb16ddff78e9714b3
dawidbazak44-ui <dawidbazak44@gmail.com>
2026-03-27 19:20:06 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T18:19:55.239Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-27T18:20:01.802Z
+Commit: Resolve null pointer exception in user service
