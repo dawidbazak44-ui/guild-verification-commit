ef788d2373fe960eb16e07c6cdb581a791e7eeff
dawidbazak44-ui <dawidbazak44@gmail.com>
2026-03-27 19:17:41 +0100

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T18:17:34.244Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-27T18:17:36.342Z
+Commit: Add multi-language support
