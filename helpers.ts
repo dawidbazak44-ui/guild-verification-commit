86466836e42dcfbe3b81558c3168a82de88ea2d1
dawidbazak44-ui <dawidbazak44@gmail.com>
2026-03-27 19:18:02 +0100

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T18:17:53.055Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-27T18:17:57.214Z
+Commit: Patch API endpoint error handling
