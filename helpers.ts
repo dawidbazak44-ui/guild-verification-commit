22ef6393b9b3dabddaeecb6f1d412a5a9452aaa0
dawidbazak44-ui <dawidbazak44@gmail.com>
2026-03-27 19:22:34 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T18:20:00.485Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-27T18:22:29.424Z
+Commit: Fix broken links in documentation
