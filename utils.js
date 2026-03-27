7841f2b2a8c48e2b6fbea0a5667d2478fe880535
dawidbazak44-ui <dawidbazak44@gmail.com>
2026-03-27 19:23:16 +0100

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T18:22:45.949Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-27T18:23:11.701Z
+Commit: Introduce real-time notifications
